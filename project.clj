1a6440b839cc07734fd077e2ee4b8df159f8853e
ChrisWs <chris07solaligue@gmail.com>
2023-12-10 16:10:23 -0500

    Se incorpora el uso de clojurescript, se realiza la creación de la busqueda por filtro, se mueve el archivo json a una ruta global.

--- project.clj
+++ project.clj
@@ -7,9 +7,27 @@
                  [ring/ring-defaults "0.3.2"]
                  [hiccup "1.0.5"]
                  [org.clojure/data.json "2.4.0"]
-                 [org.clojure/clojurescript "1.11.60"]]
-  :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler pokedex.handler/app}
+                 [org.clojure/clojurescript "1.11.60"]
+                 [cheshire "5.10.0"]
+                 [com.fasterxml.jackson.core/jackson-core "2.13.0"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.13.0"]
+                 [reagent "1.2.0"]
+                 ]
+  :plugins [
+            [lein-ring "0.12.5"]
+            [lein-cljsbuild "1.1.8"]
+            ]
+  :ring {:handler       pokedex.handler/app
+         :auto-reload?  true
+         :auto-refresh? true}
+
+  :cljsbuild {
+              :builds [{
+                        :source-paths ["src/scripts"]
+                        :compiler     {
+                                       :output-to     "resources/public/js/main.js"
+                                       :optimizations :whitespace
+                                       :pretty-print  true}}]}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})

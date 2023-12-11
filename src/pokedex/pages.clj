30a26f95e803c1ce5c7ea2703166649cacaf825e
ChrisWs <chris07solaligue@gmail.com>
2023-12-10 21:10:29 -0500

    Se realiza cambios visuales.

--- src/pokedex/pages.clj
+++ src/pokedex/pages.clj
@@ -13,8 +13,10 @@
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
      [:link {:href "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" :rel "stylesheet" :integrity "sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" :crossorigin "anonymous"}]
      [:script {:src "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" :integrity "sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" :crossorigin "anonymous"}]
+     [:link {:rel "stylesheet" :href "/css/style.css"}]
      [:title title]]
-    body
+    [:body
+     body]
     [:script {:type "text/javascript" :src "/js/main.js"}]
     ))
 
@@ -29,4 +31,5 @@
 ; La función llama a la función pokemon-info del namespace pokemon_info.clj
 (defn pokemon-info [id]
   (base-page "Información"
+             (nav/navbar-component)
              (inf/pokemon-info id)))

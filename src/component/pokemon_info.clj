a1c3d7ed60a7de4a9e7517c2461c09c5bc2bb76c
fernando <fernandozs10@hotmail.com>
2023-12-12 00:00:04 -0500

    -se refactorizo el json para almacenar los puntos de las estadisticas y la info. -se colocaron las estadisticas de manera que fueran correspondientes a los puntos de cada pokemoon. -la parte izquierda que corresponde al nombre y un poco de historia del pokemon ahora estane en un card

--- src/component/pokemon_info.clj
+++ src/component/pokemon_info.clj
@@ -6,34 +6,72 @@
 
 (defn pokemon-info [id]
   (let [pokemon-data (data-pk id)]
-    [:div.container.mx-auto.text-center
+    [:div.container-sm
      [:div.row
-      [:div.col-md-4
-       [:img.img-thumbnail {:src (get-in pokemon-data ["img"]) :alt (get-in pokemon-data ["name"])}]
-       [:img.img-thumbnail {:src (get-in pokemon-data ["img2"]) :alt (get-in pokemon-data ["name"]) :style {:margin-top "10px"}}]
-       [:a.btn.btn-primary {:href "/index" :role "button"} "Regresar"]]
-      [:div.col-md-7.text-md-start
-       [:h1.text-center (get-in pokemon-data ["name"])]
-       [:p (get-in pokemon-data ["body"])]
-       [:div.card.bg-info.text-white {:style {:width "60%" :margin "50px auto" :padding "20px"}}
+      [:div.col-4
+       [:div {:class "card text-bg-dark h-100"}
+        [:img.card-img {:src (get-in pokemon-data ["img"]) :alt (get-in pokemon-data ["name"])}]
+        [:div.card-body
+         [:h1.text-center (get-in pokemon-data ["name"])]
+         [:p.card-text (get-in pokemon-data ["body"])]]
+        [:a.btn.btn-danger {:href "/index" :role "button"} "Regresar"]
+        ]]
+      [:div.col
+       [:div.card.bg-danger.text-white.text-center
         [:div.row
-         [:div.col-md-3
-          [:p {:style {:font-size "2em" :font-weight "bold"}} "Altura:"]
-          [:p.text-dark (get-in pokemon-data ["altura"])]]
-         [:div.col-md-3
-          [:p {:style {:font-size "1.2em"}} "Categoría:"]
-          [:p.text-dark (get-in pokemon-data ["categoria"])]]
-         [:div.col-md-3
-          [:p {:style {:font-size "1.2em"}} "Peso:"]
-          [:p.text-dark (get-in pokemon-data ["peso"])]]
-         [:div.col-md-3
-          [:p {:style {:font-size "1.2em"}} "Habilidad:"]
-          [:p.text-dark (get-in pokemon-data ["habilidad"])]]
-         ]]
-       [:p {:style {:font-size "2em"}}"Tipo: "]
-       [:p.font-weight-bold (get-in pokemon-data ["tipo"])]
-       [:p {:style {:font-size "2em"}}"Debilidad: " ]
-       [:p {:class "font-weight-bold"} (get-in pokemon-data ["debilidad"])]
+         [:div.col
+          [:p {:class "fw-bold"} "Altura:"]
+          [:p.text-white (get-in pokemon-data ["info" "altura"])]]
+         [:div.col
+          [:p {:class "fw-bold"} "Categoría:"]
+          [:p.text-white (get-in pokemon-data ["info" "categoria"])]]
+         ]
+        [:div.row
+         [:div.col
+          [:p {:class "fw-bold"} "Habilidad:"]
+          [:p.text-white (get-in pokemon-data ["info" "habilidad"])]]
+         [:div.col
+          [:p {:class "fw-bold"} "Tipo:"]
+          [:p.text-white (get-in pokemon-data ["info" "tipo"])]]
+         ]
+        [:div.row
+         [:div.col
+          [:p {:class "fw-bold"} "Debilidad:"]
+          [:p.text-white (get-in pokemon-data ["info" "debilidad"])]]
+         [:div.col
+          [:p {:class "fw-bold"} "Peso:"]
+          [:p.text-white (get-in pokemon-data ["info" "peso"])]]
+         ]
+        ]
+       [:div.row.center
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Puntos de Salud: "]
+         [:div.progress.
+          [:div.progress-bar {:style (str "width: " (get-in pokemon-data ["estadisticas" "ps"]) "%")} ]]]
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Ataque: "]
+         [:div.progress.
+          [:div.progress-bar.bg-success {:style (str "width: " (get-in pokemon-data ["estadisticas" "ataque"]) "%")} ]]]
+        ]
+       [:div.row.center
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Defensa: "]
+         [:div.progress.
+          [:div.progress-bar.bg-danger {:style (str "width: " (get-in pokemon-data ["estadisticas" "defensa"]) "%")} ]]]
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Ataque Especial: "]
+         [:div.progress.
+          [:div.progress-bar.bg-warning {:style (str "width: " (get-in pokemon-data ["estadisticas" "ae"]) "%")}]]]
+        ]
+       [:div.row.center
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Defensa Especial: "]
+         [:div.progress.
+          [:div.progress-bar.bg-warning {:style (str "width: " (get-in pokemon-data ["estadisticas" "de"]) "%")} ]]]
+        [:div.col
+         [:p.text-white {:class "fw-bold"} "Velocidad: "]
+         [:div.progress.
+          [:div.progress-bar.bg-success {:style (str "width: " (get-in pokemon-data ["estadisticas" "velocidad"]) "%")}]]]
+        ]
        ]]
      ]))
-
